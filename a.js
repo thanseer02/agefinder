6d12712dde0e4580ed0badd75106dc26d1c369a8
muhammed Tanseer <112170428+thanseer02@users.noreply.github.com>
2022-11-05 16:46:50 +0530

    Update a.js

--- a.js
+++ a.js
@@ -1,9 +1,18 @@
 function age()
 {
     var a=parseInt(document.getElementById('y').value);
+    var b=parseInt(document.getElementById('m').value);
+    var c=parseInt(document.getElementById('d').value);
+
     var date = new Date ();
     var y = date.getFullYear();
-    var age = y-a;
-    document.getElementById("rs").innerHTML= "you are -"+ age +"  years old";
+    var m = date.getMonth();
+    var d = date.getDay();
+
+    var yer = y-a;
+    var mon = m-b;
+    var day = d-c;
+
+    document.getElementById("r").innerHTML= "you are -"+ yer +"- year -"+mon+"- months- "+day+"- old";
 
 }
